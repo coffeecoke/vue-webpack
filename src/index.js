c533540dd8f50ef3fac3e2176ccb388434443c67
tanghuan <18601376034@163.com>
2018-04-22 22:46:44 +0800

    0422

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './app.vue'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+new Vue ({
+    rendar: (h) => h(App)
+}).$mount(root)
