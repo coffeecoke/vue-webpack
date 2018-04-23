29b5e7dec4271974e706b54b9ba82a82e85ebc99
coffeecoke <18601376034@163.com>
2018-04-23 22:34:30 +0800

    th-0423b

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,29 @@ module.exports = {
     module: {
         rules: [
             {
-                test:/.vue$/,
+                test:/\.vue$/,
                 loader:'vue-loader'
+            },
+            {
+                text:/\.css$/,
+                use: [
+                    // 从右往左执行
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                text:/\.(gif|jpg|png|svg)$/,
+                use:[
+                    // 区别上面的数组们，这里使用对象，是因为要配置options
+                    {
+                        loader: 'url-loader',//封装了file-loader
+                        options: {
+                            limit:1024
+                        }
+
+                    }
+                ]
             }
         ]
     }
